a3ccfcba1ed4c2028ae9f15af421f80650d89541
sacha seksik <sachaseksik@hotmail.fr>
2018-10-06 20:46:55 +0200

    final
    everything is matricial
    works for XOR

--- perceptron.py
+++ perceptron.py
@@ -17,37 +17,44 @@ def front_prop(Xin,list_size_layers,weights,bias):
             list_out[col+1][per]=sigmoid(sig_in[per])
     return list_out
      
-def backprob(Xin,Yth,list_size_layers,weights,bias):
-    #backpropagation:dc/daijk=dc/dbjk *Yik-1 et dc/bik=sum dc/dbjk+1*aijk+1*Yjk+1(1-Yjk+1)
+def backprop(Xin,Yth,list_size_layers,weights,bias):
+    #backpropagation:dc/daijk=dc/dbjk *Yik-1 et dc/bik=SUMj[dc/dbjk+1*aijk+1] *Yik+1(1-Yik+1)
     #matriciellement DAk=DBk.Yk-1 et DBk=Ak+1 . DBk+1*Yk+1 *(1-Yk+1)
     list_out=front_prop(Xin,list_size_layers,weights,bias)    
     grad_weight=[weights[k]*0 for k in range(len(weights))]
     grad_bias=[bias[k]*0 for k in range(len(bias))]
     #init
     grad_bias[-1]=list_out[-1]*(np.array(Yth)-list_out[-1])*(np.ones(len(list_out[-1]))-list_out[-1])
-    grad_weight[-1]=np.dot(list_out[-2][None].T,np.array([grad_bias[-1]]))
+    grad_weight[-1]=np.dot(list_out[-2][None].T,grad_bias[-1][None])
     #recurrence
     for col in range(len(list_out)-3,-1,-1):
-        grad_bias[col]=np.dot(weights[col+1],grad_bias[col+1]*list_out[col+1]*(np.ones(len(list_out[col+1])))-list_out[col+1])
-        grad_weight[col]=np.dot(list_out[col][None].T,np.array([grad_bias[col]]))
-    return grad_weight,grad_bias
+        grad_bias[col]=np.dot(weights[col+1],grad_bias[col+1])*list_out[col+1]*(np.ones(len(list_out[col+1]))-list_out[col+1])
+        grad_weight[col]=np.dot(list_out[col][None].T,grad_bias[col][None])
+    return grad_weight,grad_bias,np.linalg.norm(Yth-list_out[-1])/2
 
 def training(listXin,listYth,list_size_layers,weights,bias,step,iterations):
-    for k in range(len(iterations)):
+    for k in range(iterations):
         delta_weight=[weights[k]*0 for k in range(len(weights))]
         delta_bias=[bias[k]*0 for k in range(len(bias))]
+        cost_tot=0
         for data in range(len(listXin)):
-            gw,gb=backprob(listXin[data],listYth[data],list_size_layers,weights,bias)
+            gw,gb,cost=backprop(listXin[data],listYth[data],list_size_layers,weights,bias)
             for col in range(len(gw)):
                 delta_weight[col]+=gw[col]*step/len(gw)
                 delta_bias[col]+=gb[col]*step/len(gw)
-        weights+=-delta_weight
-        bias+=-delta_bias
-        cost=0
-        for k in range(len(listXin)):
-            cost+=(np.linalg.norm(listXin[k]-listYth[k])**2)/2
-        print(k,cost)
-        return weights,bias
+            cost_tot+=cost
+        if k%100==0:
+            print(cost_tot)
+        for col in range(len(weights)):
+            weights[col]+=delta_weight[col]
+            bias[col]+=delta_bias[col]
+        
+    return weights,bias
 
-
-     
+#%% XOR
+list_size_layers=[3,1]       
+weights=[np.array([[0.2,-0.5,0.4],[-0.4,0.6,0.1]]),np.array([[0.3],[-0.4],[0.2]])] #line is before, column is after
+bias=[np.zeros(3),np.zeros(1)]
+listXin=np.array([[0,0],[1,0],[0,1],[1,1]])
+listYth=np.array([[0],[1],[1],[1]])
+training(listXin,listYth,list_size_layers,weights,bias,0.01,100000)
