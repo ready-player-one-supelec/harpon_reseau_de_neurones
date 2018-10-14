3eadd30be6a5a15b21266ceb6f8fc35783cde709
Martin Lehoux <martin@lehoux.net>
2018-10-14 16:27:38 +0200

    linting

--- network_base.py
+++ network_base.py
@@ -8,10 +8,13 @@ def dsigmoid(m): #Work in progress do not use
     return m * (np.ones(len(m)) - m)
 
 def tanh(x):
-    return np.tanh(x)
+    """Activation function"""
+    return 1.7159*np.tanh(2*x/3)
 
 def dtanh(m):
-    return np.ones(len(m))-m*m
+    a = 1.7159
+    b = 1/(a**2)
+    return a*(2/3)*(np.ones(len(m))-b*m*m)
 
 def front_prop(inputs, network, weights, bias, activation=sigmoid):
     """Compute the output of the network
@@ -33,8 +36,7 @@ def front_prop(inputs, network, weights, bias, activation=sigmoid):
             list_out[col+1][per] = activation(sig_in_per)
     return list_out
 
-
-def backprop(inputs, th_output, network, weights, bias, derivative=dsigmoid):
+def backprop(inputs, th_outputs, reseau, weights, bias, derivee=dsigmoid, activation=sigmoid):
     """Compute the network bias and weight gradient
     Parameters:
         inputs: List of n Arrays input of size m
@@ -44,23 +46,23 @@ def backprop(inputs, th_output, network, weights, bias, derivative=dsigmoid):
         bias: List of p Arrays of layer[k] bias
         # derivative:
     Output:
-        grad_weight: 
+        grad_weight:
         grad_bias:
 
     """
     #backpropagation:dc/daijk=dc/dbjk *Yik-1 et dc/bik=SUMj[dc/dbjk+1*aijk+1] *Yik+1(1-Yik+1)
     #matriciellement DAk=DBk.Yk-1 et DBk=Ak+1 . DBk+1*Yk+1 *(1-Yk+1)
-    list_out = front_prop(inputs, network, weights, bias)
+    list_out = front_prop(inputs, reseau, weights, bias, activation)
     grad_weight = [weights[k]*0 for k in range(len(weights))]
     grad_bias = [bias[k]*0 for k in range(len(bias))]
     #init
-    grad_bias[-1] = derivative(list_out[-1]) * (list_out[-1]-np.array(th_output))
+    grad_bias[-1] = derivee(list_out[-1]) * (list_out[-1]-np.array(th_outputs))
     grad_weight[-1] = np.dot(list_out[-2][None].T, grad_bias[-1][None])
     #recurrence
     for col in range(len(list_out)-3, -1, -1):
-        grad_bias[col] = np.dot(weights[col+1], grad_bias[col+1]) * derivative(list_out[col+1])
+        grad_bias[col] = np.dot(weights[col+1], grad_bias[col+1]) * derivee(list_out[col+1])
         grad_weight[col] = np.dot(list_out[col][None].T, grad_bias[col][None])
-    return grad_weight, grad_bias, np.linalg.norm(th_output-list_out[-1])/2
+    return grad_weight, grad_bias, np.linalg.norm(th_outputs-list_out[-1])/2
 
 def random_w_b(inputs, network):
     """Create random weights and bias for the network
