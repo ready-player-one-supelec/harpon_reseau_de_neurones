e58a8c2ec92d8a92d88dd6e7bb91d8ea088c6a2e
Kagamino <martin.lehoux@supelec.fr>
2018-10-23 09:25:31 +0200

    more general conf.py

--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@ Created on Sun Oct 14 12:18:40 2018
 
 @author: Loic
 """
-train_images_path = r"C:\Users\Loic\Documents\Projet Long HARPON\train-images.idx3-ubyte"
-train_labels_path = r"C:\Users\Loic\Documents\Projet Long HARPON\train-labels.idx1-ubyte"
-test_images_path = r"C:\Users\Loic\Documents\Projet Long HARPON\t10k-images.idx3-ubyte"
-test_labels_path = r"C:\Users\Loic\Documents\Projet Long HARPON\t10k-labels.idx1-ubyte"
+train_images_path = r"train-images.idx3-ubyte"
+train_labels_path = r"train-labels.idx1-ubyte"
+test_images_path = r"t10k-images.idx3-ubyte"
+test_labels_path = r"t10k-labels.idx1-ubyte"
